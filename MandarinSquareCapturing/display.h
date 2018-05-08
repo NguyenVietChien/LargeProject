b6e85ab7828be1f08bba4507c58ce58d73828ea5
Chien <chienuetvnu@gmail.com>
2018-05-08 20:25:50 +0700

    aaaaa

--- MandarinSquareCapturing/display.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISPLAY_H_INCLUDED
-#define DISPLAY_H_INCLUDED
-
-
-
-#endif // DISPLAY_H_INCLUDED
--- MandarinSquareCapturing/main.cpp
+++ MandarinSquareCapturing/main.cpp
@@ -24,6 +24,7 @@ void play(){
 int main()
 {
 	while(1){
+		
 		//PlaySound (TEXT ( "playmusic" ), NULL, SND_ASYNC);
     	int decide;    // replay or not ?
     	play();
